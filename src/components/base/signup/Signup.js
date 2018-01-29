1032fff7cdeeffa90bbcfae16607667744322cd7
Rafael Klynger <rafafhgkifh@gmail.com>
2018-01-28 22:14:44 -0300

    finishing form, still not done.

--- src/components/base/signup/Signup.js
+++ src/components/base/signup/Signup.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Dialog, FlatButton, TextField } from 'material-ui'
+import { Dialog, FlatButton, TextField, Avatar, FontIcon } from 'material-ui'
+import { blueA700 } from 'material-ui/styles/colors'
 import { withFormik, Form } from 'formik'
 import Yup from 'yup'
+import DropZone from 'react-dropzone'
 
 import ModalTitle from '../../shared/ModalTitle'
-
 
 const styles = {
     container: {
         width: '450px',
         maxWidth: '92%'
+    },
+    fileInput: {
+        display: 'none'
+    },
+    avatarInput: {
+        display: 'none'
     }
 }
 
@@ -21,6 +28,39 @@ class Signup extends Component {
         isOpen: PropTypes.bool.isRequired
     }
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            photo: null
+        }
+    }
+
+    handleDropImage = (e) => {
+        const { handleChange } = this.props
+
+        for (const file of e.target.files) {
+            const reader = new FileReader()
+            reader.onload = () => {
+                const fileAsDataURLString = reader.result
+                this.setState({ photo: fileAsDataURLString })
+            }
+            reader.onabort = () => console.log('file reading was aborted')
+            reader.onerror = () => console.log('file reading has failed')
+            reader.readAsDataURL(file);
+        }
+    }
+
+    handleSubmitMiddleware = (e) => {
+        // const newValues = {
+        //     ...values,
+        //     photo: this.state.photo
+        // }
+
+        console.log('passed ', e)
+        this.props.handleSubmit(e)
+    }
+
     render() {
         const {
             isOpen, onClose, values,
@@ -28,6 +68,7 @@ class Signup extends Component {
             isSubmitting, handleSubmit
         } = this.props
 
+        const { photo } = this.state
         return (
             <Dialog
                 title={<ModalTitle title="Signup" />}
@@ -46,11 +87,33 @@ class Signup extends Component {
                         secondary
                         keyboardFocused={true}
                         disabled={isSubmitting}
-                        onClick={handleSubmit}
+                        onClick={this.handleSubmitMiddleware}
                     />
                 ]}
             >
-                <Form>
+                <form >
+                    <div className="dropzone-container">
+                        <label className="dropzone-wrapper">
+                            <input
+                                name="photo"
+                                type="file"
+                                style={styles.fileInput}
+                                onChange={this.handleDropImage}
+                                // onChange={handleChange}
+                            />
+                            {photo ? <Avatar src={photo} size={150} /> : <Avatar src="/assets/no-image.jpg" size={150} />}
+                            <FontIcon className="material-icons upload-icon" color={blueA700}>file_upload</FontIcon>
+                        </label>
+                        {/* <DropZone
+                            className="dropzone-wrapper"
+                            onDrop={this.handleDropImage}
+                            multiple={false} accept="image/*"
+                            name="photo"
+                        >
+                            {values.photo ? <Avatar src={values.photo} size={150} /> : <Avatar src="/assets/no-image.jpg" size={150} />}
+                            <FontIcon className="material-icons upload-icon" color={blueA700}>file_upload</FontIcon>
+                        </DropZone> */}
+                    </div>
                     <TextField
                         name="name"
                         floatingLabelText="Name"
@@ -79,18 +142,19 @@ class Signup extends Component {
                         onChange={handleChange}
                         fullWidth
                     />
-                </Form>
+                </form>
             </Dialog>
         )
     }
 }
 
 export default withFormik({
-    mapPropsToValues({ name, email }) {
+    mapPropsToValues({ name, email, photo }) {
         return {
             name: name || '',
             email: email || '',
-            password: ''
+            password: '',
+            photo: null
         }
     },
     validationSchema: Yup.object().shape({
@@ -98,14 +162,15 @@ export default withFormik({
         email: Yup.string().email('Email not valid').required('Email is required'),
         password: Yup.string().min(4, 'Password must be 4 characters or longer').required('Password is requred')
     }),
-    handleSubmit(values, { setErrors, setSubmitting }) {
+    handleSubmit(values, formikBag) {
         setTimeout(() => {
-            if(values.email === 'rafafhgkifh@gmail.com') {
-                setErrors({ email: 'That email is already taken' })
+            if (values.email === 'rafafhgkifh@gmail.com') {
+                formikBag.setErrors({ email: 'That email is already taken' })
             }
-            
-            setSubmitting(false)
+
+            formikBag.setSubmitting(false)
         }, 2000)
         console.log(values)
+        console.log('bag', formikBag)
     }
 })(Signup)
